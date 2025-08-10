16969318aba3bc04c5f3ab53eb0c600669953362
emon22830 <mdemon22830@gmail.com>
2025-08-10 11:53:50 +0800

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import { Button } from "@/components/ui/button.jsx";
+function App() {
+  
+
+  return (
+    <>
+      <div className="my-5 mx-5">
+        <Button>Click Me </Button>
+      </div>
+    </>
+  );
+}
+
+export default App
